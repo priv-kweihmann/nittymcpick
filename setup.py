641c00d6464ad32a38f5ba7c9e6f3a98e82c40ad
Konrad Weihmann <kweihmann@outlook.com>
2020-03-01 14:47:02 +0100

    Release 1.1.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="nittymcpick",
-    version="1.1.1",
+    version="1.1.2",
     author="Konrad Weihmann",
     author_email="kweihmann@outlook.com",
     description="GitLab bot for custom linting tools to comment MRs",
